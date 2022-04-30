e5a5a322e1ea10684cade7b9e735950e90a5146b
Luis Carlos Rodriguez Valencia <lucarova@outlook.com>
2022-04-30 10:30:48 -0500

    Commit Inicial con 2 ejercicios: Lados de poligono y Suma de Enteros con formula de Gauss

---
+++ Ejercicios.py
@@ -0,0 +1,22 @@
+#EJERCICIOS DE PRACTICA
+
+# 1. Calcular diagonales de un poligono
+
+# print("Calcule las diagonales de un poligono con N lados")
+# print('')
+# lados_poligono = int(input("Ingrese el numero de lados del poligono: "))
+# resultado = int(lados_poligono**2 - 3 * lados_poligono) / 2
+# resultado = str(resultado)
+# lados_poligono = str(lados_poligono)
+# print("Las diagonales de un poligono con " + lados_poligono + " lados, son: " + resultado)
+
+# 2. Formula de Gauss
+
+print('Calcular la suma de “N” números enteros.')
+print('')
+numero_enteros = int(input('Ingrese el numero N de enteros: '))
+resultado2 = float((numero_enteros**2 + numero_enteros) / 2)
+resultado2 = round(resultado2, 2)
+print('El resultado de la suma de {} enteros con la formula\n \
+de Gauss es igual a: {}'.format(numero_enteros, resultado2))
+
