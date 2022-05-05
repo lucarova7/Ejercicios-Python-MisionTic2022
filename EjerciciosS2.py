8db9b6d95605a45e8cb4e5763084edafaca5823d
Luis Carlos Rodriguez Valencia <lucarova@outlook.com>
2022-05-05 12:04:02 -0500

    Ejercicio2 ordenar descendentemente usando el codigo anterior

--- EjerciciosS2.py
+++ EjerciciosS2.py
@@ -1,10 +1,14 @@
-#EJERCICIO1 - Ordenar ascendentemente 3 numeros dados por el usuario
+# #EJERCICIO1 - Ordenar ascendentemente 3 numeros dados por el usuario
 
-numero1 = int(input('Ingrese el primer numero: '))
-numero2 = int(input('Ingrese el segundo numero: '))
-numero3 = int(input('Ingrese el tercer numero: '))
+# numero1 = int(input('Ingrese el primer numero: '))
+# numero2 = int(input('Ingrese el segundo numero: '))
+# numero3 = int(input('Ingrese el tercer numero: '))
 
-a = min(numero1, numero2, numero3)
-b = max(numero1, numero2, numero3)
-c = (numero1 + numero2 + numero3) - a - b
-print('Los numeros ordenados ascendentemente son: {}, {}, {}'.format(a, c, b))
+# a = min(numero1, numero2, numero3)
+# b = max(numero1, numero2, numero3)
+# c = (numero1 + numero2 + numero3) - a - b
+# print('Los numeros ordenados ascendentemente son: {}, {}, {}'.format(a, c, b))
+
+# #EJERCICIO2 Ordenar descendentemente usando el codigo anterios
+
+# print('Los numeros ordenados descendentemente son: {}, {}, {}'.format(b, c, a))
