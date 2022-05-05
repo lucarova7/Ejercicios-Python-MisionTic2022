2cb2a9812beda026450c364876410d0d84301365
Luis Carlos Rodriguez Valencia <lucarova@outlook.com>
2022-05-05 13:58:00 -0500

    Ejercicio8 Calcular nota final y Actualizacion del README

--- EjerciciosS2.py
+++ EjerciciosS2.py
@@ -69,3 +69,22 @@
 # else:
 #     print('Impar')
 #     print(numero**3)
+
+#EJERCICIO8 Calcular la nota final segun las notas ingresadas
+
+#PorcentajeNota1 = 15%
+#PorcentajeNota2 = 30%
+#PorcentajeNota3 = 25%
+#PorcentajeNota4 = 10%
+#PorcentajeNota5 = 20%
+
+# nota1 = float(input('Ingrese la nota del primer examen: '))
+# nota2 = float(input('Ingrese la nota del segundo examen: '))
+# nota3 = float(input('Ingrese la nota del tercer examen: '))
+# nota4 = float(input('Ingrese la nota del cuarto examen: '))
+# nota5 = float(input('Ingrese la nota del quinto examen: '))
+
+# nota_final = nota1*0.15 + nota2*0.30 + nota3*0.25 + nota4*0.10 + nota5*0.20
+# nota_final = round(nota_final, 2)
+
+# print('La nota final del estudiante es: {}'.format(nota_final))
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Ejercicios-Python-MisionTic2022
 Compilacion de Ejercicios basicos de Python3 en la semana 2 para MisionTic2022
+Todos las lineas de codigo se encuentran comentadas ya que todos los ejercicios se realizaron en el mismo archivo.
