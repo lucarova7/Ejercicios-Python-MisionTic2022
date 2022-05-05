0ad88418370287f2d4ae278047494e682667707f
Luis Carlos Rodriguez Valencia <lucarova@outlook.com>
2022-05-05 18:14:01 -0500

    Ejercicio12 Calculo de matricula de estudiantes con o sin beneficio por promedio

--- EjerciciosS2.py
+++ EjerciciosS2.py
@@ -156,3 +156,18 @@
 #     print('Segun su resultado de IMC sufre usted de Obesidad tipo IV o extrema')
 # else:
 #     print('Ingrese nuevamente sus datos')
+
+#EJERCICIO12 Calcular el valor de la matricula con o sin beneficio por promedio
+
+valor_materias = 62000
+
+promedio = float(input('Cual fue el promedio del ultimo periodo: '))
+materias = int(input('Cuantas materias va a cursar este periodo: '))
+valor_matricula = valor_materias * materias
+IVA = valor_matricula * 0.10
+
+if promedio >= 4.5:
+    valor_matricula = valor_matricula - valor_matricula*0.30 - IVA
+    print('El valor de su matricula con descuentos por promedio es: {}'.format(valor_matricula))
+else:
+    print('El valor de su matricula sin descuentos es: ' + str(valor_matricula + IVA))
