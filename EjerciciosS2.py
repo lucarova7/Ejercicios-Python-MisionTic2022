efe096ac03ca6b7169100923d97fa6c4df9c3d1c
Luis Carlos Rodriguez Valencia <lucarova@outlook.com>
2022-05-05 13:32:00 -0500

    Ejercicio6 Saber si un año es bisiesto

--- EjerciciosS2.py
+++ EjerciciosS2.py
@@ -48,3 +48,12 @@
 #     print('Escriba UNA letra')
 # else:
 #     print('No es vocal')
+
+#EJERCICIO6 Saber si un año es bisiesto
+
+# año = int(input('Ingrese un año: '))
+
+# if año % 4 == 0 or año % 400 == 0 and año % 100 != 0:
+#     print('Es bisiesto')
+# else:
+#     print('No es bisiesto')
