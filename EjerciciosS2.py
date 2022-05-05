6b5ca06318941b0029c3d434ba5b875f106bd442
Luis Carlos Rodriguez Valencia <lucarova@outlook.com>
2022-05-05 12:06:02 -0500

    Ejercicio3 Saber si un numero ingresado por el usuario esta en el rango [-3, 27]

--- EjerciciosS2.py
+++ EjerciciosS2.py
@@ -12,3 +12,13 @@
 # #EJERCICIO2 Ordenar descendentemente usando el codigo anterios
 
 # print('Los numeros ordenados descendentemente son: {}, {}, {}'.format(b, c, a))
+
+#EJERCICIO3 Saber si un numero ingresado por el usuario esta en el rango [-3, 27]
+
+# intervalo = range(-3, 28)
+
+# numero = int(input('Inserte un numero: '))
+# if numero in intervalo:
+#     print('Este numero si pertenece al intervalo [-3, 27]')
+# else:
+#     print('Este numero no pertenece al intervalo [-3, 27]')
