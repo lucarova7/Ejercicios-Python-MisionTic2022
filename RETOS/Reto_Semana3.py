26de6a3049ec48e3ac2978812b0caf5cf4b42a6e
Luis Carlos Rodriguez Valencia <lucarova@outlook.com>
2022-05-15 13:54:59 -0500

    Reto de la tercera semana. Añadir ciclo for

---
+++ RETOS/Reto_Semana3.py
@@ -0,0 +1,17 @@
+numeroPaquetes = int(input())
+costoTotal = 0
+
+for __ in range(numeroPaquetes):
+    alto = float(input())
+    ancho = float(input())
+    profundo = float(input())
+    volumen = alto * ancho * profundo
+    costo = volumen * 5
+    if alto > 30:
+        costo = costo + 2000
+    if costo > 10000:
+            costo = costo + costo*0.19
+    print(volumen)
+    print(costo)
+    costoTotal = costo+costoTotal
+print(costoTotal)
