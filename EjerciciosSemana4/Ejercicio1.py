304220a286830a30b11f25c50d9bc5484d3cab0a
Luis Carlos Rodriguez Valencia <lucarova@outlook.com>
2022-05-28 12:45:01 -0500

    Añado calculo de volumen

--- EjerciciosSemana4/Ejercicio1.py
+++ EjerciciosSemana4/Ejercicio1.py
@@ -1,8 +1,25 @@
-def multiplicacion(lista):
-    resultado = 1
-    for i in lista:
-        resultado = resultado * i
-    return resultado
+# #Funcion con condicional
 
-print(multiplicacion([2,2,5]))
+# def procesar_paquete(peso, volumen):
+#     if peso < 2 and volumen < 0.027:
+#         return True
+#     else:
+#         return False
 
+# print(procesar_paquete(1, 0.01))
+
+
+#Mejora incluyendo el calculo del volumen
+
+def procesar_paquete(peso):
+    alto = float(input('Ingresa el alto del paquete (cm): \n'))
+    ancho = float(input('Ingresa el ancho del paquete (cm): \n'))
+    profundo = float(input('Ingresa la profundidad del paquete (cm): \n'))
+    volumen = (alto * ancho * profundo) / 1000000
+    volumen = round(volumen, 2)
+    if peso < 50 and volumen < 0.027:
+        return print('El paquete si es elegible para envio')
+    else:
+        return print('El paquete no puede ser enviado')
+
+procesar_paquete(6)
